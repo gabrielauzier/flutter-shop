e6007c7dac5b9d5f7111a1f539e5b155b10dc7b1
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-11-04 13:16:16 -0300

    feat: using provider (#224)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/core/app_colors.dart';
 import 'package:shop/core/app_routes.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/products_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
-import 'package:shop/providers/counter.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: theme.copyWith(
