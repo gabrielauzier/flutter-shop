d52474f88210f39ec9353012b9cc76255ab3dd8c
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-11-04 13:37:59 -0300

    feat: marking product as favorite (#226)

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/core/app_routes.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/pages/products_detail_page.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  const ProductItem({
-    super.key,
-    required this.product,
-  });
+  const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -23,8 +20,11 @@ class ProductItem extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).colorScheme.secondary,
           ),
           trailing: IconButton(
