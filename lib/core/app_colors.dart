2ec6bfed917e2d2c9547fa7fe39821812389d505
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-11-09 22:20:35 -0300

    feat: filter favorites (#230)

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final Color primary = Colors.red;
-  static final Color secondary = Colors.amber;
+  static const Color primary = Colors.red;
+  static const Color secondary = Colors.amber;
+  static const Color surface = Colors.white70;
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_grid.dart';
 import 'package:shop/core/app_colors.dart';
+import 'package:shop/models/product_list.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
+      backgroundColor: AppColors.surface,
       appBar: AppBar(
         title: const Text('Minha loja'),
         backgroundColor: AppColors.primary,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
-      body: ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
